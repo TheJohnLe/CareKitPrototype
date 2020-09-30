544b01bbf5971790f542bb6451ec47a51b54950e
John Le <jle17@mail.sfsu.edu>
2020-09-29 22:05:39 -0700

    Initial Commit

---
+++ CareKitPrototype/CareKitPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareKitPrototypeApp.swift
+//  CareKitPrototype
+//
+//  Created by John Le on 9/29/20.
+//
+
+import SwiftUI
+
+@main
+struct CareKitPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
